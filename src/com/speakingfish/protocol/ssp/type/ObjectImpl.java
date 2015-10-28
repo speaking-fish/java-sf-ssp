27120f4c929f57d2de5f3218db88c0b75ee20aaf
speaking-fish <speakingfish@gmail.com>
2015-10-28 22:18:58 +0400

    minor: fix warning

--- src/com/speakingfish/protocol/ssp/type/ObjectImpl.java
+++ src/com/speakingfish/protocol/ssp/type/ObjectImpl.java
@@ -3,7 +3,6 @@ package com.speakingfish.protocol.ssp.type;
 import java.io.OutputStream;
 import java.util.AbstractList;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 
