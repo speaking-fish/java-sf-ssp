d436272fe126b0af8cd7cb18d35e63e0f3d94f9e
borka <borka@localhost>
2016-03-24 12:33:12 +0300

    +warn on LocalAny usage without LocalNamed

--- src/com/speakingfish/protocol/ssp/LocalAny.java
+++ src/com/speakingfish/protocol/ssp/LocalAny.java
@@ -1,6 +1,7 @@
 package com.speakingfish.protocol.ssp;
 
 import com.speakingfish.common.type.LocalNamed;
+import com.speakingfish.common.type.Named;
 
 public interface LocalAny<CONTEXT, TYPE> extends Any<TYPE> {
 /*
@@ -10,6 +11,12 @@ public interface LocalAny<CONTEXT, TYPE> extends Any<TYPE> {
     <SUBCONTEXT, T> LocalAny<SUBCONTEXT, T> item   (LocalNamed<CONTEXT, LocalNamed<SUBCONTEXT, T>> name              );
     <SUBCONTEXT, T> T                       value  (LocalNamed<CONTEXT, LocalNamed<SUBCONTEXT, T>> name              );
 */
+    @Deprecated <T> int    add    (Named<T> name, Any<T> value);
+    @Deprecated <T> Any<T> update (Named<T> name, Any<T> value);
+    @Deprecated <T> Any<T> remove (Named<T> name              );
+    @Deprecated <T> Any<T> item   (Named<T> name              );
+    @Deprecated <T> T      value  (Named<T> name              );
+    
     <            T> int                     add    (LocalNamed<CONTEXT, T> name, Any<T> value);
     <SUBCONTEXT, T> LocalAny<SUBCONTEXT, T> update (LocalNamed<CONTEXT, T> name, Any<T> value);
     <SUBCONTEXT, T> LocalAny<SUBCONTEXT, T> remove (LocalNamed<CONTEXT, T> name              );
